2b6938b927b52eb04544605d9c956e2abd22c663
eviw817 <evelien.derudder@gmail.com>
2024-01-10 23:14:07 +0100

    making an API and endpoints

---
+++ craft-exam/config/element-api.php
@@ -0,0 +1,72 @@
+<?php
+
+use craft\elements\Entry;
+use craft\helpers\UrlHelper;
+use craft\helpers\MoneyHelper;
+
+return [
+    'endpoints' => [
+        'api/flowers' => function() {
+            return [
+                'elementType' => Entry::class,
+                'criteria' => ['section' => 'flowers'],
+                'cache' => false,
+                'transformer' => function(Entry $entry) {
+                    $pictures = [];
+                    foreach ($entry->picture->all() as $pic) {
+                        $pictures[] = $pic->url;
+                    }
+
+                    $colors = [];
+                    foreach ($entry -> referenceColor -> all() as $color) {
+                        $colors[] = [
+                            'name' => $color-> title,
+                            'hex' => $color-> color-> getHex()
+                        ];
+                    }
+                    return [
+                        'title' => $entry->title,
+                        'pictures' => $pictures,
+                        'colors' => $colors,
+                        'leaves' => $entry -> leaves,
+                        'price' => MoneyHelper::toString($entry -> price),
+                        'info' => $entry -> information,
+                        'bloomingSeason' => $entry -> bloomingSeason,
+                        'slug' => $entry -> slug
+                    ];
+                },
+            ];
+        },
+        'api/vases' => function() {
+            return [
+                'elementType' => Entry::class,
+                'criteria' => ['section' => 'vases'],
+                'cache' => false,
+                'transformer' => function(Entry $entry) {
+                    $pictures = [];
+                    foreach ($entry->picture->all() as $pic) {
+                        $pictures[] = $pic->url;
+                    }
+
+                    $colors = [];
+                    foreach ($entry -> referenceColor -> all() as $color) {
+                            $colors[] = [
+                            'name' => $color-> title,
+                            'hex' => $color-> color -> getHex()
+                        ];
+                    }
+
+                    return [
+                        'title' => $entry-> title,
+                        'pictures' => $pictures,
+                        'colors' => $colors,
+                        'info' => $entry -> information,
+                        'price' => MoneyHelper::toString($entry -> price),
+                        'manufacturer' => $entry -> manufacturer,
+                        'slug' => $entry -> slug
+                    ];
+                }
+            ];
+        },
+    ]
+];
